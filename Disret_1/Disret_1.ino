560ea688b5b3e5ba3742171e6a78c589fe478005
Aleksandr-Git <gavryukov@mail.ru>
2018-09-08 23:07:05 +0300

    Добавлены новые шлейфы

--- Disret_1/Disret_1.ino
+++ Disret_1/Disret_1.ino
@@ -6,17 +6,17 @@ Bounce debouncer_2 = Bounce();
 Bounce debouncer_3 = Bounce();
 
 //const int analogInPin = A0;
-const int dPin_0 = 0;
-const int dPin_1 = 1;
-const int dPin_2 = 2;
-const int dPin_3 = 3;
+const int dPin_0 = 0; // контроль электропитания вводного щита
+const int dPin_1 = 1; // защита от протечки
+const int dPin_2 = 2; // датчик движения
+const int dPin_3 = 3; // включение режима охраны
 //int dValue;
 int LED = 13;
 
 boolean flag_0 = true;
 boolean flag_1 = true;
 boolean flag_2 = true;
-boolean flag_3 = true;
+int flag_3 = 0;
 
 void setup() {
 pinMode(dPin_0, INPUT);
@@ -48,40 +48,60 @@ void loop() {
 //dValue = digitalRead(dPin_0);
 
   if (debouncer_0.read() == HIGH && flag_0 == true){ // если значение HIGH
-    Serial.println("high_0");
+    Serial.println("Norma_D0");
     flag_0 = false;
     }
   if (debouncer_0.read() == LOW && flag_0 == false){ // если значение LOW
-    Serial.println("low_0");
+    Serial.println("Alarm_D0");
     flag_0 = true;
     }
 
   if (debouncer_1.read() == HIGH && flag_1 == true){ // если значение HIGH
-    Serial.println("high_1");
+    Serial.println("Norma_D1");
     flag_1 = false;
     }
   if (debouncer_1.read() == LOW && flag_1 == false){ // если значение LOW
-    Serial.println("low_1");
+    Serial.println("Alarm_D1");
     flag_1 = true;
     }
 
-  if (debouncer_2.read() == HIGH && flag_2 == true){ // если значение HIGH
-    Serial.println("high_2");
+  if (debouncer_2.read() == HIGH && flag_2 == true && flag_3 == 2){ // если значение HIGH
+    Serial.println("Norma_D2");
     flag_2 = false;
     }
-  if (debouncer_2.read() == LOW && flag_2 == false){ // если значение LOW
-    Serial.println("low_2");
+  if (debouncer_2.read() == LOW && flag_2 == false && flag_3 == 2){ // если значение LOW
+    Serial.println("Alarm_D2");
     flag_2 = true;
     }
 
-  if (debouncer_3.read() == HIGH && flag_3 == true){ // если значение HIGH
-    Serial.println("high_3");
-    flag_3 = false;
-    }
-  if (debouncer_3.read() == LOW && flag_3 == false){ // если значение LOW
-    Serial.println("low_3");
-    flag_3 = true;
-    }
+  if (debouncer_3.read() == HIGH && flag_3 == 0){
+    Serial.println("Mode_M");
+    flag_3 = 1;
+  }
+
+  else if (debouncer_3.read() == LOW && flag_3 == 1){
+    flag_3 = 2;
+  }
+  
+  else if (debouncer_3.read() == HIGH && flag_3 == 2){
+    Serial.println("Mode_A");
+    flag_3 = 3;
+  }
+
+  else if (debouncer_3.read() == LOW && flag_3 == 3){
+    flag_3 = 0;
+  }
+
+
+//  if (debouncer_3.read() == HIGH && flag_3 == true){ // если значение HIGH
+//    Serial.println("Norma_D3");
+//    flag_3 = false;
+//    }
+//  if (debouncer_3.read() == LOW && flag_3 == false){ // если значение LOW
+//    Serial.println("Alarm_D3");
+//    flag_3 = true;
+//    }
+
 //if (digitalRead(dPin_0) == HIGH){
 //  digitalWrite(LED, !digitalRead(LED));
 //  Serial.println("SOUND");
